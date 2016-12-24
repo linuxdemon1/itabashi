10a40e7786b3513775e8a4154770be00d99420f5
linuxdemon1 <walter@walterbarnes.net>
2016-12-24 04:12:16 -0600

    Restructured Event class

--- itabashi/event.py
+++ itabashi/event.py
@@ -1,20 +1,44 @@
+import enum
+
+from bot import RelayBot
 from link import RelayLink
 
 
+@enum.unique
+class EventType(enum.Enum):
+    message = 0
+    action = 1
+    other = 6
+
+
 class Event:
-    def __init__(self, _type: str, nick: str, chan: str, conn: RelayLink):
-        self.type = _type
-        self.nick = nick
-        self.chan = chan
+    def __init__(self, *, bot: RelayBot = None, conn: RelayLink = None, base_event=None,
+                 event_type: EventType = EventType.other, nick: str = None, chan: str = None):
+        self.bot = bot
         self.conn = conn
+        if base_event is not None:
+            if self.bot is None and base_event.bot is not None:
+                self.bot = base_event.bot
+            if self.conn is None and base_event.conn is not None:
+                self.conn = base_event.conn
+            self.type = base_event.type
+            self.nick = base_event.nick
+            self.chan = base_event.chan
+        else:
+            self.type = event_type
+            self.nick = nick
+            self.chan = chan
 
 
 class MessageEvent(Event):
-    def __init__(self, nick: str, chan: str, conn: RelayLink, message: str, *, _type: str = "message"):
-        super().__init__(_type, nick, chan, conn)
+    def __init__(self, *, bot: RelayBot = None, conn: RelayLink = None, base_event=None, nick: str = None,
+                 chan: str = None, message: str = None):
+        super().__init__(bot=bot, conn=conn, base_event=base_event, event_type=EventType.message, nick=nick, chan=chan)
         self.message = message
 
 
-class ActionEvent(MessageEvent):
-    def __init__(self, nick: str, chan: str, conn: RelayLink, message: str):
-        super().__init__(nick, chan, conn, message, _type="action")
+class ActionEvent(Event):
+    def __init__(self, *, bot: RelayBot = None, conn: RelayLink = None, base_event=None, nick: str = None,
+                 chan: str = None, message: str = None):
+        super().__init__(bot=bot, conn=conn, base_event=base_event, event_type=EventType.action, nick=nick, chan=chan)
+        self.message = message
